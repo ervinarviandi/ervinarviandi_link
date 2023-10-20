c98b53437c87be75c3fe53eff4dcc94236989256
Ervin Arviandi <muhamadervin34@gmail.com>
2023-10-20 04:51:05 -0700

    update 0.4

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -13,7 +13,7 @@ const Links = () => {
         <div className="grid md:grid-cols-2 gap-5 mt-4">
           <div className="flex flex-col">
             <a href="https://ervinarvian.vercel.app/" aria-label="right icons">
-              <div className="border-2 border-gray-200 p-4 rounded-lg relative hover:bg-gradient-to-r from-green-200 to-purple-300 duration-300 ease-in" aria-label="link pembungkus personal portfolio">
+              <div className="border-2 border-gray-200 p-4 rounded-lg relative hover:bg-gradient-to-r from-green-200 to-purple-300 duration-300 ease-in" aria-label="link pembungkus personal portfolio" role="link">
                 <div className="flex  items-center gap-x-3">
                   <BiPin className="text-gray-400 text-lg" />
                   <h1 className="font-manrope text-lg text-black md:w-full w-8/12">Personal Website & Portfolio</h1>
@@ -26,7 +26,7 @@ const Links = () => {
           </div>
           <div className="flex flex-col">
             <a href="https://app.gitbook.com/o/vXWd1hsKDlQ1x5wo7ASa/s/AoqsT3g63kOfLrnoT3nF/untitled/astro-introduction">
-              <div className="border-2 border-gray-200 p-4 rounded-lg relative hover:bg-gradient-to-r from-green-200 to-purple-300 duration-300 ease-in" aria-label="link pembungkus blog">
+              <div className="border-2 border-gray-200 p-4 rounded-lg relative hover:bg-gradient-to-r from-green-200 to-purple-300 duration-300 ease-in" aria-label="link pembungkus blog" role="link">
                 <div className="flex  items-center gap-x-3">
                   <FaBlog className="text-gray-400 text-lg" />
                   <h1 className="font-manrope text-lg text-black">My Blog </h1>
@@ -39,7 +39,7 @@ const Links = () => {
           </div>
           <div className="flex flex-col">
             <a href="https://trakteer.id/ervinarviandi" aria-label="trakteer link">
-              <div className="border-2 border-gray-200 p-4 rounded-lg relative  hover:bg-gradient-to-r from-green-200 to-purple-300 duration-300 ease-in" aria-label="link pembungkus buy me a cup coffee">
+              <div className="border-2 border-gray-200 p-4 rounded-lg relative  hover:bg-gradient-to-r from-green-200 to-purple-300 duration-300 ease-in" aria-label="link pembungkus buy me a cup coffee" role="link">
                 <div className="flex  items-center gap-x-3">
                   <BiCoffee className="text-gray-400 text-lg" />
                   <h1 className="font-manrope text-lg text-black">Buy me a Cup of Coffee </h1>
